73ea27ee8d8e1abfbee48baccfa52680e85b3ae8
AmmadmJaved <ammad.mjaved@gmail.com>
2025-08-17 08:41:38 +0500

    update

--- server/db.ts
+++ server/db.ts
@@ -7,7 +7,7 @@ neonConfig.webSocketConstructor = ws;
 console.log('DATABASE_URL:', process.env.DATABASE_URL);
 if (!process.env.DATABASE_URL) {
   throw new Error(
-    "DATABASE_URL must be set. Did you forget to provision a database?",
+    "DATABASE_URL must be set. Did you forget to provision a database.?",
   );
 }
 
